af2f21412efa71672690b97c303a86814a3b6225
Jobu <111178322+Job-u@users.noreply.github.com>
2025-06-18 17:48:07 +0800

    Add files via upload

---
+++ BroCode_JAVA/Package1/A.java
@@ -0,0 +1,8 @@
+package Package1;
+import Package2.*;
+public class A {
+	
+	protected String protectedcMessage = "This is the protected"; //Only accessible as long as the Class has a sub class or uses extends
+
+
+}
---
+++ BroCode_JAVA/Package1/B.java
@@ -0,0 +1,9 @@
+package Package1;
+import Package2.*;
+public class B {
+
+	private   String privateMessage = "This is private";		  //Only visible to the class the it contain itself
+	
+	//only the class B can use or access this privateMessage
+	
+}
